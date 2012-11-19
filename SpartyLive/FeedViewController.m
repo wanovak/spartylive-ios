7932ae5a319c2b30fc25aa5886277afa98a2255a
Will <wanovak@gmail.com>
2012-11-19 11:36:24 -0500

    JSON Parsing

--- SpartyLive/FeedViewController.m
+++ SpartyLive/FeedViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FeedViewController.h"
+#import "application.h"
+#import "JSONKit.h"
 
 @implementation FeedViewController
 
@@ -38,6 +40,51 @@
         [self.logoImageView setFrame:CGRectMake(self.logoImageView.frame.origin.x, self.logoImageView.frame.origin.y, 300, 300)];
     }
                      completion:^(BOOL finished){  }];
+    
+    
+    // Send request to API
+    NSString *urlAsString = APIURL @"/Feed/latest/";
+    NSURL *url = [NSURL URLWithString:urlAsString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection
+     sendAsynchronousRequest: urlRequest
+     queue: [[NSOperationQueue alloc] init]
+     completionHandler:^(NSURLResponse *reponse,
+                         NSData *data,
+                         NSError *error)
+     {
+         if([data length] > 0 && error == nil)
+         {
+             // Result successfully received
+             NSString *jsonString = [[NSString alloc] initWithData: data encoding:NSUTF8StringEncoding];
+             NSLog(@"Here is what we got: %@", jsonString);
+             NSDictionary *results = [jsonString objectFromJSONString];
+             NSArray *resArr = [results objectForKey:@"data"];
+             
+             for(NSDictionary *record in resArr)
+             {
+                 UITableView *tableView;
+                 UITableViewCell *cell = [tableView
+                                          dequeueReusableCellWithIdentifier:@"FeedCell"];
+                 
+                 NSString *message = [record objectForKey:@"message"];
+                 NSString *firstname = [record objectForKey:@"firstname"];
+                 cell.textLabel.text = message;
+                 cell.detailTextLabel.text = firstname;
+                 NSLog(@"First name: %@", firstname);
+             }
+         }
+         else if ([data length] == 0 && error == nil)
+         {
+             NSLog(@"Nothing was downloaded.");
+         }
+         else if (error != nil)
+         {
+             NSLog(@"Error = %@", error);
+         }
+     }];
+      
 }
 
 - (void)viewDidUnload
---
+++ SpartyLive/application.h
@@ -0,0 +1,1 @@
+#define APIURL @"http://devapi.spartylive.com/v1"
